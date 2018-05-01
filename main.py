6515e62cb0952643dd814f578a90a7bd75f0a2da
Alex Gain <doctorjuice1@gmail.com>
2018-04-30 21:09:32 -0400

    Adj update working; CNN preprocessing working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.utils.data
+import torch.nn.functional as F
 
 import struct
 from copy import deepcopy, copy
@@ -10,6 +11,9 @@ from time import time, sleep
 import gc
 
 from sklearn.preprocessing import normalize
+
+import matplotlib.pyplot as plt
+
 ##import warnings
 ##warnings.filterwarnings("ignore")
 
@@ -28,10 +32,43 @@ def read_idx(filename):
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
 
-xtest = read_idx('t10k-images.idx3-ubyte')
-ytest = read_idx('t10k-labels.idx1-ubyte')
-xtrain = read_idx('train-images.idx3-ubyte')
-ytrain = read_idx('train-labels.idx1-ubyte')
+##xtest = read_idx('t10k-images.idx3-ubyte')
+##ytest = read_idx('t10k-labels.idx1-ubyte')
+##xtrain = read_idx('train-images.idx3-ubyte')
+##ytrain = read_idx('train-labels.idx1-ubyte')
+
+
+def load_mnist(path, kind='train'):
+    import os
+    import gzip
+    import numpy as np
+
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte.gz'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte.gz'
+                               % kind)
+
+    with gzip.open(labels_path, 'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
+                               offset=8)
+
+    with gzip.open(images_path, 'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
+                               offset=16).reshape(len(labels), 784)
+
+    return images, labels
+
+xtrain, ytrain = load_mnist('data/fashion', kind='train')
+xtest, ytest = load_mnist('data/fashion', kind='t10k')
+
+#xtest = read_idx('t10k-images-idx3-ubyte.gz')
+#ytest = read_idx('t10k-labels-idx1-ubyte.gz')
+#xtrain = read_idx('train-images-idx3-ubyte.gz')
+#ytrain = read_idx('train-labels-idx1-ubyte.gz')
+
 
 ##when using softmax:
 def to_categorical(y, num_classes):
@@ -41,8 +78,8 @@ ytrain = to_categorical(ytrain, 10)
 ytest = to_categorical(ytest, 10)
 
 ##reshaping data:
-xtrain = np.reshape(xtrain,(xtrain.shape[0],784))
-xtest = np.reshape(xtest,(xtest.shape[0],784))
+xtrain = np.reshape(xtrain,(xtrain.shape[0],1,28,28))
+xtest = np.reshape(xtest,(xtest.shape[0],1,28,28))
 xtrain = xtrain.astype('float32')
 xtest = xtest.astype('float32')
 xtrain /= 255
@@ -61,41 +98,6 @@ test = torch.utils.data.TensorDataset(xtest, ytest)
 ###                        ###
 
 class RangeNormalize(object):
-    """
-    Given min_val: (R, G, B) and max_val: (R,G,B),
-    will normalize each channel of the th.*Tensor to
-    the provided min and max values.
-    Works by calculating :
-        a = (max'-min')/(max-min)
-        b = max' - a * max
-        new_value = a * value + b
-    where min' & max' are given values, 
-    and min & max are observed min/max for each channel
-    
-    Arguments
-    ---------
-    min_range : float or integer
-        Min value to which tensors will be normalized
-    max_range : float or integer
-        Max value to which tensors will be normalized
-    fixed_min : float or integer
-        Give this value if every sample has the same min (max) and 
-        you know for sure what it is. For instance, if you
-        have an image then you know the min value will be 0 and the
-        max value will be 255. Otherwise, the min/max value will be
-        calculated for each individual sample and this will decrease
-        speed. Dont use this if each sample has a different min/max.
-    fixed_max :float or integer
-        See above
-    Example:
-        >>> x = th.rand(3,5,5)
-        >>> rn = RangeNormalize((0,0,10),(1,1,11))
-        >>> x_norm = rn(x)
-    Also works with just one value for min/max:
-        >>> x = th.rand(3,5,5)
-        >>> rn = RangeNormalize(0,1)
-        >>> x_norm = rn(x)
-    """
     def __init__(self, 
                  min_val, 
                  max_val):
@@ -124,68 +126,147 @@ class RangeNormalize(object):
 
 normalize_01 = RangeNormalize(0,1) #needed for torch tensor normalization between 0 and 1
 
-###                      ###
-### Define torch network ###
-###                      ###
+###                                    ###
+### Define torch preprocessing network ###
+###                                    ###
+
+class ff_Net(nn.Module):
+    def __init__(self):
+        super(ff_Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc2(x))
+        return x
+
+###                         ###
+### Define main torch model ###
+###                         ###
+
+##some hyper-parameters:
+input_size = 28*28
+net_size = 250
+num_classes = 10
+
 
 class Net(nn.Module):
-    def __init__(self, adj_net, adj_in, W_net, W_in, dna, W_dna):
+    def __init__(self, adj_net = None, adj_in = None, W_net = None, W_in = None, dna = None, W_dna = None, ff_net = None):
         super(Net, self).__init__()
 
-        #basic building block inits:
-##        self.adj_net = adj_net
-        self.adj_in = adj_in
-        self.W_net = W_net
-        self.W_in = W_in
-        self.W_dna = W_dna
-        self.dna = dna
+        ###initializing adjacency matrices and weights:
 
+        ##network adjacency and weights:
+        self.adj_net = torch.round(torch.Tensor(net_size, net_size).uniform_(0, 1))
+        self.W_net = torch.Tensor(net_size, net_size).normal_(0, 0.01)
+
+        ##input adjacency and weights:
+        self.adj_in = torch.round(torch.Tensor(net_size, input_size).uniform_(0, 1))
+        self.W_in = torch.Tensor(net_size, input_size).normal_(0, 0.01)
+
+        ##changing tensors to nn.Parameters/Variables:
         if cuda_boole:
-            self.state = Variable(torch.Tensor(adj_net.shape[0],1).uniform_(0, 0.1).cuda())
+        ##    adj_net, W_net, adj_in, W_in = Variable(adj_net.cuda(), requires_grad = False), nn.Parameter(W_net.cuda(), requires_grad = True), Variable(adj_in.cuda(), requires_grad = False), nn.Parameter(W_in.cuda(), requires_grad = True)
+            self.adj_net, self.W_net, self.adj_in, self.W_in = Variable(self.adj_net.cuda(), requires_grad = True), nn.Parameter(self.W_net.cuda(), requires_grad = True), Variable(self.adj_in.cuda(), requires_grad = True), nn.Parameter(self.W_in.cuda(), requires_grad = True)
         else:
-            self.state = Variable(torch.Tensor(adj_net.shape[0],1).uniform_(0, 0.1))
-            
-        self.prev_state = copy(self.state)
+        ##    adj_net, W_net, adj_in, W_in = Variable(adj_net, requires_grad = False), nn.Parameter(W_net, requires_grad = True), Variable(adj_in, requires_grad = False), nn.Parameter(W_in, requires_grad = True)
+            self.adj_net, self.W_net, self.adj_in, self.W_in = Variable(self.adj_net, requires_grad = True), nn.Parameter(self.W_net, requires_grad = True), Variable(self.adj_in, requires_grad = True), nn.Parameter(self.W_in, requires_grad = True)
+
+        ###initializing W_dna and dna:
+
+        #computing via random uniform:
+        self.W_dna = torch.Tensor(net_size,net_size).uniform_(0, 1)
+        self.dna = torch.Tensor(net_size,net_size).uniform_(0, 1)
+
+        #changing to dna stuff to nn.Parameters/Variables:
+        if cuda_boole:
+            self.W_dna = nn.Parameter(self.W_dna.cuda(), requires_grad = True)
+            self.dna = Variable(self.dna.cuda(), requires_grad = False)
+        else:
+            self.W_dna = nn.Parameter(self.W_dna, requires_grad = True)
+            self.dna = Variable(self.dna, requires_grad = False)
+
+        seed = torch.Tensor(self.adj_net.shape[0]).uniform_(0, 0.1)
         
+        if cuda_boole:
+            self.state = Variable(seed.cuda(), requires_grad = True)
+        else:
+            self.state = Variable(seed, requires_grad = True)
+                    
         #dense out layer needed for classification:
-##        self.ff_dna_net = nn.Linear(500, self.adj_net.shape[0]*self.adj_net.shape[1], bias = True).cuda()
-        self.ff_out = nn.Linear(self.state.shape[0], 10, bias = True) #output     
+        self.ff_out = nn.Linear(self.state.shape[0], 10, bias = False) #output     
         
         #activations:
         self.relu = nn.ReLU()
         self.sm = nn.Softmax()
 
-        #re-computing adj_net:
-##        self.adj_net = torch.round(normalize_01(self.relu(dna)))
-##        self.adj_net = self.adj_net.resize(self.W_net.shape[0],self.W_net.shape[1])
-##        self.adj_net = torch.round(normalize_01(self.relu(self.W_dna.mm(self.dna))))
+        ##Manually setting initialization:
+        if adj_net is not None:
+            self.adj_net = adj_net
+        if adj_in is not None:
+            self.adj_in = adj_in
+        if W_net is not None:
+            self.W_net = W_net
+        if W_in is not None:
+            self.W_in = W_in
+        if W_dna is not None:
+            self.W_dna = W_dna
+        if dna is not None:
+            self.dna = dna
+        if ff_net is not None:
+            self.ff_net = ff_net
+            out_shape = list(self.ff_net.modules())[-1]
+            out_shape = out_shape.out_features
+            self.adj_in, self.W_in = None, None
+            self.adj_in = nn.Parameter(torch.round(torch.Tensor(net_size, out_shape).uniform_(0, 1)))
+            self.W_in = nn.Parameter(torch.Tensor(net_size, out_shape).normal_(0, 0.01))
+            
+        else:
+            self.ff_net = None
+
         
-    def update(self, input_data):
-
-        #main update equation and computation:
-##        new_state = self.relu((self.adj_net*self.W_net).mm(self.prev_state) + (self.adj_in*self.W_in).mm(torch.transpose(input_data,0,1)))
-        new_state = self.relu((torch.round(normalize_01(self.relu(self.W_dna.mm(self.dna))))*self.W_net).mm(self.prev_state) + (self.adj_in*self.W_in).mm(torch.transpose(input_data,0,1)))
-
-        #updating variables:
-        self.prev_state = copy(self.state)
-        self.state = new_state
-
-        return new_state
-
     def forward(self, input_data):
 
-        if input_data.shape[0] != self.state.shape[0]:
-            if cuda_boole:
-                self.state = Variable(torch.Tensor(adj_net.shape[0],input_data.shape[0]).uniform_(0, 0.1).cuda())
-            else:
-                self.state = Variable(torch.Tensor(adj_net.shape[0],input_data.shape[0]).uniform_(0, 0.1))
-            self.prev_state = copy(self.state)
-    
-        for k in range(3):
-            self.update(input_data)
+        ##Doing pre-processing if ff_net is available:
+        if self.ff_net is not None:
+            input_data = self.ff_net(input_data)
 
-        return self.sm(self.ff_out(torch.transpose(self.state,0,1)))
+        ##Setting initial state for each datapoint:
+        seed = torch.Tensor(input_data.shape[0],self.adj_net.shape[0]).uniform_(0, 0.1)
+
+        if cuda_boole:
+            self.state = Variable(seed.cuda(), requires_grad = True)
+        else:
+            self.state = Variable(seed, requires_grad = True)
         
+        ##Updating adj_net (and adj_in?):
+        self.adj_net = normalize_01(self.relu(self.W_dna.mm(self.dna)))
+        self.adj_net = (1 / (1+torch.exp(-(80*(self.adj_net - 0.5)))))
+
+        ##Forward propagation:
+        x_bias = ((self.adj_in*self.W_in).matmul(torch.transpose(input_data,0,1)))
+
+        x = self.relu((self.adj_net*self.W_net).matmul(self.state.t())) + x_bias
+        x = self.relu((self.adj_net*self.W_net).matmul(x)) + x_bias
+        x = self.relu((self.adj_net*self.W_net).matmul(x)) + x_bias
+                
+        return self.sm(self.ff_out(x.t()))
+        
+    def plot_adj(self):
+        self.adj_net = normalize_01(self.relu(self.W_dna.mm(self.dna)))
+        self.adj_net = (1 / (1+torch.exp(-(80*(self.adj_net - 0.5)))))
+
+        plt.imshow(self.adj_net.cpu().data.numpy())
+        plt.show()
+
     def save_weights(self, file_name):
         params = list(self.parameters())
         L = len(params)-2
@@ -196,65 +277,19 @@ class Net(nn.Module):
             Ws = [w.numpy().T for w in params]
         np.savez(file_name, args = [w for w in Ws])
 
-###hyper-parameters:
-input_size = 28*28
-net_size = 500
-num_classes = 10
-
-###initializing adjacency matrices and weights:
-
-##network adjacency and weights:
-adj_net = torch.round(torch.Tensor(net_size, net_size).uniform_(0, 1))
-#adj_net = adj_net + adj_net.transpose(0,1) - adj_net.diag()
-W_net = torch.Tensor(net_size, net_size).normal_(0, 0.01)
-
-##input adjacency and weights:
-adj_in = torch.round(torch.Tensor(net_size, input_size).uniform_(0, 1))
-#adj_in = adj_in + adj_in.transpose(0,1) - adj_in.diag()
-W_in = torch.Tensor(net_size, input_size).normal_(0, 0.01)
-
-##changing tensors to Variables:
-if cuda_boole:
-    adj_net, W_net, adj_in, W_in = Variable(adj_net.cuda(), requires_grad = False), Variable(W_net.cuda(), requires_grad = True), Variable(adj_in.cuda(), requires_grad = False), Variable(W_in.cuda(), requires_grad = True)
-else:
-    adj_net, W_net, adj_in, W_in = Variable(adj_net, requires_grad = False), Variable(W_net, requires_grad = True), Variable(adj_in, requires_grad = False), Variable(W_in, requires_grad = True)
-
-###initializing W_dna and dna:
-
-#computing via random uniform:
-W_dna = torch.Tensor(500,500).uniform_(0, 1)
-dna = torch.Tensor(500,500).uniform_(0, 1)
-
-#changing to dna stuff to Variables:
-if cuda_boole:
-    W_dna = Variable(W_dna.cuda(), requires_grad = True)
-    dna = Variable(dna.cuda(), requires_grad = False)
-else:
-    W_dna = Variable(W_dna, requires_grad = True)
-    dna = Variable(dna, requires_grad = False)
-
-#computing adj_net:
-def relu_tensor(x):
-    return x*((x>0).float())
-
-##adj_net = Variable(torch.round(normalize_01(relu_tensor(W_dna.mm(dna)))).cuda(), requires_grad = False)
-##adj_net = torch.round(normalize_01(nn.ReLU()(W_dna.mm(dna)))).cuda()
-
 ###defining network:
-my_net = Net(adj_net, adj_in, W_net, W_in, dna, W_dna)
 if cuda_boole:
-    my_net.cuda()
-
-
-###test:
-##inputyo = Variable(torch.Tensor(784,1).uniform_(0,1))
-##out = my_net.update(inputyo)
+    ff_net = ff_Net().cuda()
+    my_net = Net(ff_net = ff_net).cuda()
+else:
+    ff_net = ff_Net()
+    my_net = Net(ff_net = ff_net)
 
 ###                       ###
 ### Loss and optimization ###
 ###                       ###
 
-LR = 0.01
+LR = 0.0001
 loss_metric = nn.MSELoss()
 optimizer = torch.optim.Adam(my_net.parameters(), lr = LR)
 
@@ -265,7 +300,7 @@ optimizer = torch.optim.Adam(my_net.parameters(), lr = LR)
 #Some more hyper-params and initializations:
 epochs = 30
 N = xtrain.shape[0]
-BS = 32
+BS = 128
 
 train_loader = torch.utils.data.DataLoader(train, batch_size=BS, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test, batch_size=BS, shuffle=False)
@@ -277,7 +312,7 @@ total = 0
 for images, labels in train_loader:
     if cuda_boole:
         images, labels = images.cuda(), labels.cuda()
-    images = Variable(images.view(-1, 28*28))
+    images = Variable(images)#.view(-1, 28*28))
     outputs = my_net(images)
     _, predicted = torch.max(outputs.data, 1)
     labels = torch.max(labels.float(),1)[1]
@@ -294,7 +329,7 @@ total = 0
 for images, labels in test_loader:
     if cuda_boole:
         images, labels = images.cuda(), labels.cuda()
-    images = Variable(images.view(-1, 28*28))
+    images = Variable(images)#.view(-1, 28*28))
     outputs = my_net(images)
     _, predicted = torch.max(outputs.data, 1)
     labels = torch.max(labels.float(),1)[1]
@@ -330,7 +365,14 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         outputs = my_net.forward(x)
         loss = loss_metric(outputs,y)
-        loss.backward()
+        loss.backward(retain_graph = True)
+
+##        my_net.W_dna.grad = my_net.W_dna.grad*(1e10)
+##        my_net.W_in.grad = 0*my_net.W_in.grad
+##        my_net.W_net.grad = 0*my_net.W_net.grad
+##        my_net.ff_out.weight.grad = 0*my_net.ff_out.weight.grad
+##        my_net.ff_out.weight.bias = 0*my_net.ff_out.weight.bias
+        
 
 ##        print(my_net.W_dna)
                 
@@ -338,7 +380,7 @@ for epoch in range(epochs):
         optimizer.step()
         
         ##printing statistics:
-        if (i+1) % 1875 == 0:
+        if (i+1) % np.floor(N/BS) == 0:
             print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' 
                    %(epoch+1, epochs, i+1, N//BS, loss.data[0]))
 
@@ -349,7 +391,7 @@ for epoch in range(epochs):
             for images, labels in train_loader:
                 if cuda_boole:
                     images, labels = images.cuda(), labels.cuda()
-                images = Variable(images.view(-1, 28*28))
+                images = Variable(images)#.view(-1, 28*28))
                 outputs = my_net(images)
                 _, predicted = torch.max(outputs.data, 1)
                 labels = torch.max(labels.float(),1)[1]
@@ -366,7 +408,7 @@ for epoch in range(epochs):
             for images, labels in test_loader:
                 if cuda_boole:
                     images, labels = images.cuda(), labels.cuda()
-                images = Variable(images.view(-1, 28*28))
+                images = Variable(images)#.view(-1, 28*28))
                 outputs = my_net(images)
                 _, predicted = torch.max(outputs.data, 1)
                 labels = torch.max(labels.float(),1)[1]
@@ -375,6 +417,13 @@ for epoch in range(epochs):
                 correct += (predicted.float() == labels.float()).sum()
 
             print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+            yes_adj = (my_net.adj_net>=0.9).float().sum().cpu().data.numpy()[0] / net_size**2
+            no_adj = (my_net.adj_net<=0.1).float().sum().cpu().data.numpy()[0] / net_size**2
+            print('% greater than 0.9:',yes_adj)
+            print('% less than 0.1:',no_adj)
+            print('Sum:',yes_adj + no_adj)
+
 
 
     ##time-keeping 2:
@@ -386,5 +435,4 @@ for epoch in range(epochs):
 t2 = time()
 print((t2 - t1)/60,'total minutes elapsed')
 
-
-
+    
